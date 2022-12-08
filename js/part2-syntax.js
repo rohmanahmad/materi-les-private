e94d333c0a0b535fa79bc697409604dd1a308247
rohmanahmad <rohmanmail@gmail.com>
2022-12-08 21:29:46 +0700

    bahasan syntax

---
+++ js/part2-syntax.js
@@ -0,0 +1,16 @@
+/* 
+    syntax/perintah
+    => digunakan untuk memerintahkan/menginstruksikan
+        pada browser untuk melakukan suatu perintah program.
+*/
+/* 
+contoh syntax:
+- pem-variabelan
+    variable pada js itu teridiri dari 3 metode pemvariabelan. antaralain:
+    - menggunakan keyword "var"
+        contoh: var luas = 500; // bisa diubah(sifatnya var bisa dideklarasikan di akhir, meski nama variable dipanggil sebelum pemvariabelan tsb)
+    - menggunakan keyword "let"
+        contoh: let luas = 500; // bisa diubah(sifatnya wajib di deklarasikan terlebih dahulu sebelum dipanggil)
+    - menggunakan keyword "const"
+        contoh: const luas = 500; // nilai constan tidak dapat diubah
+*/
