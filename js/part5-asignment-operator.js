33447cc4bb68b702bc94aa029e85415aebf955da
rohmanahmad <rohmanmail@gmail.com>
2022-12-29 21:03:20 +0700

    add assingment operator

---
+++ js/part5-asignment-operator.js
@@ -0,0 +1,32 @@
+/* 
+    Asignment Operator Yang Berarti (operator penugasan),
+        dimana suatu nilai akan diberikan kepada suatu variabel
+        dengan menambahkan operasi aritmatika pada variabel tersebut
+
+    Operasi Aritmatika yg diperbolehkan yaitu:
+        - = (sama dengan)
+        - += (nilai pd "variable assigment" akan ditambahkan dengan variabel setelahnya)
+            ex: a += b (nilai "a" adalah nilai dari "a" + "b")
+        - -= (nilai pd "variable assigment" akan dikurangkan dengan variabel setelahnya)
+            ex: a -= b (nilai "a" adalah nilai dari "a" - "b")
+        - *= (nilai pd "variable assigment" akan dikalikan dengan variabel setelahnya)
+            ex: a *= b (nilai "a" adalah nilai dari "a" * "b")
+        - *= (nilai pd "variable assigment" akan dibagi dengan variabel setelahnya)
+            ex: a /= b (nilai "a" adalah nilai dari "a" / "b")
+        - %= (nilai pd "variable assigment" akan disisabagikan dengan variabel setelahnya)
+            ex: a /= b (nilai "a" adalah nilai dari "a" % "b")
+        - **= (nilai pd "variable assigment" akan dipangkatkan dengan variabel setelahnya)
+            ex: a **= b (nilai "a" adalah nilai dari "a" ^ "b")
+*/
+
+/* Contoh pada operasi (+=) */
+let x = 10;
+let y = 5;
+x += y; // x = (x + y)
+
+/* Contoh pada operasi (+=) */
+let xx = 10;
+let yy = 5;
+xx -= yy; // xx = (xx - yy)
+
+// dan seterusnya (langsung lihat praktek, dan bandingkan pada penggunaannya)
