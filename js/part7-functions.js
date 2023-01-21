95e8b3a8f8ee2f1ba7f0e041e7e7067d0e9da155
rohmanahmad <rohmanmail@gmail.com>
2023-01-21 20:33:47 +0700

    add materi function pertemuan #1

---
+++ js/part7-functions.js
@@ -0,0 +1,40 @@
+/**
+ * function / fungsi adalah suatu baris code yg digunakan untuk memecahkan suatu masalah yg spesifik,
+ * sehingga dibuat function tsb dengan nama yg inline dengan masalah yang akan diselesaikan.
+ * - pada suatu function tidak diharuskan untuk membalikkan / return suatu nilai.
+ * 
+ * contoh:
+ * masalah: ingin membuat pemecahan masalah mengenai perkalian 2 bilangan.
+ * jadi solusinya: buat function dengan nama addTwoNumber(n1, n2)
+ */
+
+// contoh penulisan function
+// contoh 1
+function aTambahB(a, b) { // named function / fungsi yg mempunyai nama
+    // code here
+}
+// cara call function
+const hasil = aTambahB(1, 3)
+
+// contoh 2
+const cTambahD = function (c, d) { // disebut anonimous function
+    // code here
+}
+// cara call function
+const hasil2 = cTambahD(1, 3)
+
+// contoh 3
+const eTambahF = (e, f) => { // disebut anonimous arrow function
+    // code here
+}
+// cara call function
+const hasil3 = eTambahF(1, 3)
+
+// contoh 4
+const xPlusY = function namaFunction (x, y) { // named function / fungsi yg mempunyai nama
+    // code here
+}
+// cara call function
+const hasil4 = xPlusY(1, 3)
+
+
