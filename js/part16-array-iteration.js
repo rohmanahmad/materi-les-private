efa7f880b333bb8fe542c93da32400336dc9f44c
rohmanahmad <rohmanmail@gmail.com>
2023-02-24 20:11:15 +0700

    reduce

--- js/part16-array-iteration.js
+++ js/part16-array-iteration.js
@@ -1,10 +1,10 @@
 /* 
 Array Iteration merupakan fungsi yang digunakan untuk meng-iterasi / perulangan
 
-- .map
-- .forEach
-- .filter
-- .reduce
+- .map >> mempunyai return
+- .forEach >> (void) tidak mempunyai return
+- .filter >> mempunyai return
+- .reduce >> mempunyai return
 - .reduceRight
 - .every
 - .some
@@ -46,5 +46,61 @@ const filterResultGenap1 = array1.filter(function (value) {
     const isGenap = (value % 2 === 0)
     return isGenap
 })
-console.log(filterResultGanjil1)
-console.log(filterResultGenap1)
+// console.log(filterResultGanjil1)
+// console.log(filterResultGenap1)
+
+// .reduce
+const array2 = [
+    {key: 'a', value: 'AA', count: 1, group: 1},
+    {key: 'b', value: 'BB', count: 3, group: 2},
+    {key: 'c', value: 'CC', count: 2, group: 1},
+    {key: 'd', value: 'DD', count: 4, group: 1}
+]
+// variabel result = {a: 'AA', b: 'BB', dst}
+// (1) value dari field "key" akan menjadi key dari "result(variable)"
+// (2) value dari field "value" akan menjadi value dari poin 1
+const reduce1 = array2.reduce(function (result, item) {
+    const k = item.key
+    const v = item.value
+    result[k] = v
+    return result
+}, {})
+console.log('reduce1', reduce1) // >>> { a: 'AA', b: 'BB', c: 'CC', d: 'DD' }
+const reduce2 = array2.reduce(function (result, item){
+    const k = item.group
+    const v = item.count
+    if (result[k]) result[k] += v
+    else result[k] = v
+    return result
+}, {})
+console.log('reduce2', reduce2) // >> { '1': 7, '2': 3 }
+
+const reduce3 = array2.reduce(function (result, item) {
+    result += item.value
+    return result
+}, '')
+console.log('reduce3', reduce3) // >> AABBCCDD
+
+const reduce4 = array2.reduce(function (result, item){
+    const arrValues = [item.key, item.value, item.count]
+    const k = item.group
+    if (!result[k]) result[k] = []
+    result[k].push(arrValues)
+    return result
+}, {})
+console.log(reduce4) 
+/* 
+result4:
+{
+  '1': [ [ 'a', 'AA', 1 ], [ 'c', 'CC', 2 ], [ 'd', 'DD', 4 ] ],
+  '2': [ [ 'b', 'BB', 3 ] ]
+}
+*/
+
+// let result = {}
+// for (const item of array2) {
+//     const k = item.key
+//     const v = item.value
+//     result[k] = v
+// }
+// console.log('for-loop', result)
