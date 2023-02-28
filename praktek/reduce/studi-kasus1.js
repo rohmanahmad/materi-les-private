7a3539a74276fa3f6cbd2b14b8f933faa83cefc3
rohmanahmad <rohmanmail@gmail.com>
2023-02-28 19:55:32 +0700

    update studi kasus

---
+++ praktek/reduce/studi-kasus1.js
@@ -0,0 +1,61 @@
+const dataFromDB = [
+    {
+        kelas: 'I',
+        nama_wali_kelas: 'Pak Edwin .Sos',
+        nama_ketua_kelas: 'Mr. Danny',
+        nama_anggota_kelas: [
+            'Aarav',
+            'Abian',
+            'Abraham',
+            'Aciel',
+            'Adam',
+            'Adelard',
+            'Adelio',
+            'Aditya',
+            'Adnan',
+        ]
+    },
+    {
+        kelas: 'II',
+        nama_wali_kelas: 'Pak Dharmendra .SI',
+        nama_ketua_kelas: 'Mr. Denver',
+        nama_anggota_kelas: [
+            'Adrian',
+            'Adya',
+            'Agam',
+            'Aileen',
+            'Aldari',
+            'Alexi',
+            'Ansel',
+            'Arsenio',
+            'Arlo',
+            'Axel',
+            'Aziel',
+        ]
+    }
+]
+
+/* 
+[
+    'Aarav kelas II',
+    'Abian kelas II',
+    'Abraham kelas II',
+    'Aciel kelas II',
+    'Adam kelas II',
+    'Adelard kelas II',
+    'Adelio kelas II',
+    'Aditya kelas II',
+    'Adnan kelas II',
+    'Adrian kelas I',
+    'Adya kelas I',
+    'Agam kelas I',
+    'Aileen kelas I',
+    'Aldari kelas I',
+    'Alexi kelas I',
+    'Ansel kelas I',
+    'Arsenio kelas I',
+    'Arlo kelas I',
+    'Axel kelas I',
+    'Aziel kelas I',
+]
+*/
