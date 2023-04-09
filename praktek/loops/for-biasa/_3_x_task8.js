aa6ff0f3e0f5f90d2e35b6b24aa7c138d678278d
greenwam <wildan.ikbar@gmail.com>
2023-04-09 19:31:55 +0700

    task 8

--- praktek/loops/for-biasa/_3_x_task8.js
+++ praktek/loops/for-biasa/_3_x_task8.js
@@ -10,7 +10,19 @@ const data = [1,2,3,4,5,6,7]
 const x = 0
 const y = data.length
 const nilaiMax = 7
-const reversed = data.reverse()
+const map = data.map(o=>o)
+const reversed = map.reverse()
 for (let i = x; i < y; i++) {
     // code here
+    size = reversed[i]
+    size2 = data[i]
+    const Star = '*'.repeat(size)
+    const RevStar = '*'.repeat(y-size+1)
+    const Space = ' '.repeat(nilaiMax-size)
+    const RevSpace = ' '.repeat(y-size2)
+    const Mirror = '*'.repeat(size-1)
+    const RevMirror = '*'.repeat(y-size)
+    const Segi31 = Space + Star + Mirror + Space
+    const Segi32 = RevSpace + RevStar + RevMirror + RevSpace
+    console.log(Segi31 + Segi32)
 }
