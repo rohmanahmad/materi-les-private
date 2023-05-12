46592e2e1b4a9ecee84f9f75bcee7b755c2ae269
rohmanahmad <rohmanmail@gmail.com>
2023-05-12 19:55:00 +0700

    update task untuk refresh knowledge

---
+++ praktek/tgl-12/tes.js
@@ -0,0 +1,23 @@
+/* 
+========= 1
+const statement = 'apa kabar,'
+const name = 'wildan'
+
+>>> perintah:
+- buat sebuah kalimat dari variabel diatas menggunakan operator "+" sehingga menjadi sebuah kalimat "apa kabar, wildan?"
+- rancang kalimat dari kedua variabel diatas menjadi spt poin 1 akan tetapi menggunakan string template (lihat part13 untuk contohnya)
+
+========= 2
+const a = 'H'
+const b = 'E'
+const c = 'L'
+const d = 'L'
+const e = 'O'
+
+>>> perintah:
+- gabungkan variabel diatas menjadi array spt berikut:
+['H','E','L','L','O']
+- transform / ubah hasil dari poin 1 menjadi tipe Data String menggunakan method .join sehingga menjadi 'HELLO'
+- transform / ubah hasil dari poin 1 menjadi tipe Data String menggunakan method .join sehingga menjadi 'H E L L O'
+
+*/
