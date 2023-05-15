3904e98f7068e9ae76482973e2291d9f5b390e68
rohmanahmad <rohmanmail@gmail.com>
2023-05-15 15:37:23 +0700

    update chanllenge

---
+++ challenges/14-mei/data.js
@@ -0,0 +1,26 @@
+/* 
+ubah urutan data tersebut menggunakan fungsi .sort(), .reverse()
+*/
+const dataAwal = [1,2,5,3,5,6,7,8,9,1,2]
+/* 
+hasilnya harus
+ASC => [1, 1, 2, 2, 3,5, 5, 6, 7, 8,9]
+DESC => [9, 8, 7, 6, 5, 5, 3, 2, 2, 1, 1]
+*/
+// tulis code nya pada file challenges/14-mei/answers/ans-1.js
+
+
+/* 
+mapping data menggunakan fungsi .map()
+hasilnya harus:
+[
+  { a1: 1 }, { a2: 2 },
+  { a5: 5 }, { a3: 3 },
+  { a5: 5 }, { a6: 6 },
+  { a7: 7 }, { a8: 8 },
+  { a9: 9 }, { a1: 1 },
+  { a2: 2 }
+]
+*/
+const data = [1,2,5,3,5,6,7,8,9,1,2]
+// tulis code nya pada file challenges/14-mei/answers/ans-2.js
